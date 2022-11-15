e6338bef7901a6db4f7bdde8eb62cbeef19ea248
Daniel Godin <danielgodin@Daniels-MacBook-Air.local>
2022-11-15 12:15:20 -0800

    btn listener event working with alert().  Now need to add function inside to display winner/loser

--- js-script.js
+++ js-script.js
@@ -6,6 +6,34 @@ function getComputerChoice() {
     let gameArray = ['Rock', 'Paper', 'Scissors'];  
     return gameArray[Math.floor((Math.random() * gameArray.length))];
 }
+const btn = document.querySelectorAll('button');
+
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert(button.id);
+    });
+});
+
+
+
+// btn.addEventListener('click', () => {
+//     alert("Hello World!");
+//     let computerSelection = getComputerChoice();
+//     let playerSelection = btn.value; // No idea how to do this yet.
+
+//     playRound(playerSelection, computerSelection);
+    
+//     if (playRound(playerSelection, computerSelection) === `playerWinsRound`) {
+//         ++numberOfPlayerWins;
+//         console.log(`You won this round!  ${playerSelection} beats ${computerSelection}.  Score: Player: ${numberOfPlayerWins} vs. Computer: ${numberOfComputerWins}.`)
+//     } else if (playRound(playerSelection, computerSelection) === `tieRound`) {
+//         console.log(`You tied this round.  You both chose ${playerSelection}.  Score: Player: ${numberOfPlayerWins} vs. Computer: ${numberOfComputerWins}.`); 
+//     } else {
+//         ++numberOfComputerWins;
+//         console.log(`You lose this round.  ${computerSelection} beats ${playerSelection}.  Score: Player: ${numberOfPlayerWins} vs. Computer: ${numberOfComputerWins}.`);
+//     }
+
+// });
 
 // Function to play Rock, Paper, Scissors 5 times.  Keep Track Of Number of Wins.  Display Number of Wins.  Use prompt() to get user input and verify it is one of the allowable answers.
 function game() {
@@ -51,35 +79,35 @@ function game() {
 // Function should use prompt() to get player choice.
 // Function should be case-insensitive.  First, lowercase everything, then uppercase the first letter.
 // Function should verify that player has written "rock, paper, or scissors" (case-insensitive).  If player has not, let player write in choice again.  Remind player to type carefully and that they have 3 chances to input their answer correctly.
-function getPlayerChoice () {
-    let playerSelection = prompt("Rock, Paper, or Scissors?", "").toLowerCase(); // Prompt to have player choose "Rock, Paper, or Scissors?", then lowercase answer.
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1); // Changing playerSelection variable to have the first letter be capitalized.
+// function getPlayerChoice () {
+//     let playerSelection = prompt("Rock, Paper, or Scissors?", "").toLowerCase(); // Prompt to have player choose "Rock, Paper, or Scissors?", then lowercase answer.
+//     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1); // Changing playerSelection variable to have the first letter be capitalized.
 
-// Verification:  
-// If player inputs "Rock, Paper, or Scissors" into the prompt(), then return that value.  That value then gets used in the game() and playRound() functins.
-// If player does not spell "Rock, Paper, or Scissors" correctly, they should be prompted to input their choice again.  Remind player to verify their answer is spelled correctly.  Give player 3 chances before they lose the round.
-    const acceptedAnswers = ["Rock", "Paper", "Scissors"]; // Array of "accepted" answers.  This array should only be called after player input is all lowercased and then first letter only is uppercased.
+// // Verification:  
+// // If player inputs "Rock, Paper, or Scissors" into the prompt(), then return that value.  That value then gets used in the game() and playRound() functins.
+// // If player does not spell "Rock, Paper, or Scissors" correctly, they should be prompted to input their choice again.  Remind player to verify their answer is spelled correctly.  Give player 3 chances before they lose the round.
+//     const acceptedAnswers = ["Rock", "Paper", "Scissors"]; // Array of "accepted" answers.  This array should only be called after player input is all lowercased and then first letter only is uppercased.
 
-    if (acceptedAnswers.includes(playerSelection)) {
-        return playerSelection;
-    } else {
-        // For loop that loops 3 times if the original prompt is not spelled correctly.  Each loop prompts a special message to check spelling.  Each loop checks if their new answer includes one of three allowable answers.  If it does not, the loop runs again.  If it does, it returns the value.  If loop runs 3 times, it returns an undefined (i think) answer, which will make the player lose the round.
-        for (let i = 0; i < 3; i++) {
-            let playerSelection = prompt("Incompatible Answer.  Check Your Spelling.  Would you like Rock, Paper, or Scissors?", "").toLowerCase();
-            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+//     if (acceptedAnswers.includes(playerSelection)) {
+//         return playerSelection;
+//     } else {
+//         // For loop that loops 3 times if the original prompt is not spelled correctly.  Each loop prompts a special message to check spelling.  Each loop checks if their new answer includes one of three allowable answers.  If it does not, the loop runs again.  If it does, it returns the value.  If loop runs 3 times, it returns an undefined (i think) answer, which will make the player lose the round.
+//         for (let i = 0; i < 3; i++) {
+//             let playerSelection = prompt("Incompatible Answer.  Check Your Spelling.  Would you like Rock, Paper, or Scissors?", "").toLowerCase();
+//             playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 
-                if (acceptedAnswers.includes(playerSelection)) {
-                    return playerSelection;
-            }
-        }
-    }
-}
+//                 if (acceptedAnswers.includes(playerSelection)) {
+//                     return playerSelection;
+//             }
+//         }
+//     }
+// }
 
 // Function to play a single round of the game Rock, Paper, Scissors.
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "Rock" && computerSelection === "Scissors" || 
-        playerSelection === "Paper" && computerSelection === "Rock" ||
-        playerSelection === "Scissors" && computerSelection === "Paper") {
+    if (playerSelection === "rock" && computerSelection === "scissors" || 
+        playerSelection === "paper" && computerSelection === "rock" ||
+        playerSelection === "scissors" && computerSelection === "paper") {
             return `playerWinsRound`;
     } else if (playerSelection === computerSelection) {
             return `tieRound`;
